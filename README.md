c7f0ab7bd6f8890b55dc91bb49cb8818f89580b0
Rubén Sánchez Rivero <slnruben750@gmail.com>
2016-03-31 14:37:44 +0200

    Este commit es la v1 de History API

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # X-Nav-5.7.18-HistoryAPI
 Ejercicio 5.7.18 - History API - Cambiando la historia con HTML5
+
+<a href="http://slnruben.github.io/X-Nav-5.7.18-HistoryAPI">History API</a>
--- gallery.js
+++ gallery.js
@@ -5,7 +5,7 @@ function supports_history_api() {
 function swapPhoto(href) {
   var req = new XMLHttpRequest();
   req.open("GET",
-           "http://gsyc.es/~grex/history_api/gallery/" +
+           "gallery/" +
              href.split("/").pop(),
            false);
   req.send(null);
